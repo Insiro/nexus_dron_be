8cbc20f2264ce41abb829c66faffd8d759ee67f1
Insiro <leeon@insiro.me>
2023-05-22 22:30:17 +0900

    add: initialize Repositories

---
+++ nexus_drone_drone/src/main/java/repository/DroneTypeRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import com.nexus.drone.drone.domain.model.DroneType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface DroneTypeRepository extends JpaRepository<DroneType, UUID> {
+}
