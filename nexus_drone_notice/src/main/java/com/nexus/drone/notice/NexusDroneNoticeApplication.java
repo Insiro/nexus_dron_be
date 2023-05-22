b52a9daa70031dfb7ba899f144e511dd003ab9d1
Insiro <leeon@insiro.me>
2023-05-22 21:58:02 +0900

    add: Notice Service Initalize

---
+++ nexus_drone_notice/src/main/java/com/nexus/drone/notice/NexusDroneNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.drone.notice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusDroneNoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusDroneNoticeApplication.class, args);
+    }
+
+}
