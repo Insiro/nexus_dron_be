ce2fd91a33c828a3bc2035f4a591bdcd9a8f69a0
klego99 <klego98@gmail.com>
2023-05-23 15:59:08 +0900

    add: userDefaultdata

--- nexus_drone_user/src/main/java/com/nexus/drone/user/domain/User.java
+++ nexus_drone_user/src/main/java/com/nexus/drone/user/domain/User.java
@@ -1,10 +1,7 @@
 package com.nexus.drone.user.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.io.Serializable;
@@ -13,16 +10,17 @@ import java.io.Serializable;
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
 @Entity
+@Builder
 public class User implements Serializable {
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long uuid;
-//    @ColumnDefault("1")
-    private int permission;
+    @Builder.Default
+    private int permission=1;
     private String ID;
     private String pwd;
-//    @ColumnDefault("https://swingbrowser.userecho.com/s/cache/b5/09/b5098d61f3ef34b35311456b08123897.png")
-    private String img;
+    @Builder.Default
+    private String img="https://swingbrowser.userecho.com/s/cache/b5/09/b5098d61f3ef34b35311456b08123897.png";
 }
 
