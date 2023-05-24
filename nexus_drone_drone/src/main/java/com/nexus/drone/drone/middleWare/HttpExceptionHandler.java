e443378cd9538172d6c86149bc9af94c0e0bac0c
Insiro <leeon@insiro.me>
2023-05-24 12:52:58 +0900

    add: Exception Handling With MethodArgumentTypeMismatchException

--- nexus_drone_drone/src/main/java/com/nexus/drone/drone/middleWare/HttpExceptionHandler.java
+++ nexus_drone_drone/src/main/java/com/nexus/drone/drone/middleWare/HttpExceptionHandler.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.NoSuchElementException;
-
 @ControllerAdvice
 public class HttpExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
@@ -14,9 +14,19 @@ public class HttpExceptionHandler {
         HttpStatus status  = HttpStatus.NOT_FOUND;
         return new ResponseEntity<>(status.getReasonPhrase(), status);
     }
-
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(MethodArgumentTypeMismatchException e){
+        String message = e.getMessage();
+        HttpStatus status;
+        if (message.contains("UUID"))
+            status = HttpStatus.NOT_FOUND;
+        else
+            status = HttpStatus.UNPROCESSABLE_ENTITY;
+        return new ResponseEntity<>(status.getReasonPhrase(), status);
+    }
     @ExceptionHandler(Throwable.class)
-    public ResponseEntity<String> handleException(Throwable ignoredE) {
+    public ResponseEntity<String> handleException(Throwable e) {
+        e.printStackTrace();
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         return new ResponseEntity<>(status.getReasonPhrase(), status);
     }
