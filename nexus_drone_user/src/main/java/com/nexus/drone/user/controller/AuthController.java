1461fd1b777716c3106db824b055eabaa30c0255
klego99 <81143217+klego99@users.noreply.github.com>
2023-05-24 23:26:51 +0900

    fix: login method return value =>token

--- nexus_drone_user/src/main/java/com/nexus/drone/user/controller/AuthController.java
+++ nexus_drone_user/src/main/java/com/nexus/drone/user/controller/AuthController.java
@@ -1,17 +1,13 @@
 package com.nexus.drone.user.controller;
 
-import com.nexus.drone.user.Token.Token;
-import com.nexus.drone.user.TokenServiceFeignClient;
 import com.nexus.drone.user.domain.User;
 import com.nexus.drone.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
 
 @CrossOrigin("http://localhost:3000")
@@ -19,55 +15,73 @@ import java.util.List;
 public class AuthController {
     @Autowired
     private final UserRepository userRepository;
-    TokenServiceFeignClient tokenServiceFeignClient;
 
     public AuthController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @PostMapping("/api/auth")
-    public ResponseEntity<Token> login(@RequestBody User user) {
+    public ResponseEntity<String> login(@RequestBody User user) {
         if (userRepository.findByID(user.getID()) == null) {
             //아이디가 없을때
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            return
+                    new ResponseEntity<>("ID_not_Exist", HttpStatus.NOT_FOUND);
         } else {
             if (!userRepository.findByID(user.getID()).getPwd().equals(user.getPwd()))
-                return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-            //비밀번호 없을때
-            else
-                return new ResponseEntity<>(tokenServiceFeignClient.getToken(), HttpStatus.OK);
-            //통과
+                return
+                        new ResponseEntity<>("pwd_wrong", HttpStatus.NOT_ACCEPTABLE);
+
+                //비밀번호 없을때
+            else {
+                String url = "http://localhost:18010/auth/getToken";
+                RestTemplate restTemplate = new RestTemplate();
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                User user1 = userRepository.findByID(user.getID());
+                HashMap<String, String> map = new HashMap<>();
+                map.put("UUID", String.valueOf(user1.getUuid()));
+                map.put("Role", String.valueOf(user1.getPermission()));
+                HttpEntity<HashMap<String, String>> request =
+                        new HttpEntity<>(map, headers);
+                return restTemplate.postForEntity(url, request, String.class);
+
+            }//통과
         }
     }
+
     @GetMapping("/api/auth/{ID}")
-    public User logininfo(@PathVariable("ID") String ID){
+    public User logininfo(@PathVariable("ID") String ID) {
         return userRepository.findByID(ID);
     }
+
     @PostMapping("/api/auth/register")
-    public int register(@RequestBody User user){
-        if(userRepository.findByID(user.getID())!=null)
+    public int register(@RequestBody User user) {
+        if (userRepository.findByID(user.getID()) != null)
             return 0;
-        else{
+        else {
             userRepository.save(user);
             return 1;
         }
     }
+
     @PutMapping("api/user/{uuid}")
-    public User changeInfo(@PathVariable("uuid") Long uuid,@RequestBody User user){
-        User user1=userRepository.findById(uuid).get();
+    public User changeInfo(@PathVariable("uuid") Long uuid, @RequestBody User user) {
+        User user1 = userRepository.findById(uuid).get();
         user1.setID(user.getID());
         user1.setPwd(user.getPwd());
-        if(user.getImg()!=null)
+        if (user.getImg() != null)
             user1.setImg(user.getImg());
         userRepository.save(user1);
         return user1;
     }
+
     @GetMapping("/api/user/{uuid}")
-    public User userinfo(@PathVariable("uuid") Long uuid){
+    public User userinfo(@PathVariable("uuid") Long uuid) {
         return userRepository.findById(uuid).get();
     }
+
     @GetMapping("/api/user")
-    public List<User> AllUser(){
+    public List<User> AllUser() {
         return userRepository.findAll();
     }
 }
